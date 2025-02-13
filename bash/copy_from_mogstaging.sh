f15e52a7c7c22084d5ef2d6961f365e07630d9b9
c0ntact0 <4921241+c0ntact0@users.noreply.github>
2025-02-13 20:06:20 +0000

    default commit

--- bash/copy_from_mogstaging.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-open smb://mog-staging.pluralpp.plr/d$
-rsync -ruv "/Volumes/d$/LTC_PROJECT/*" "/Users/rui/LTC_PROJECT/."
--- bash/copy_to_mogstaging.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-open smb://mog-staging.pluralpp.plr/d$
-rsync -ruv "/Users/rui/LTC_PROJECT/"* /Volumes/d$/LTC_PROJECT/.
